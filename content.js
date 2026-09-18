7010fc4f26cb0654af95fdd3509001b88a8abd1e
HaoChen <hc4@student.unimelb.edu.au>
2026-09-17 23:07:26 -0400

    feat: add Instagram contact link

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // Profile content is separate from the project catalogue.
 const CONTENT = {
-  email:'jarrett.chen217@gmail.com', github:'https://github.com/JarrettChen217', linkedin:'https://www.linkedin.com/in/chen-hao-j33625/', wechat:'Che8147',
+  email:'jarrett.chen217@gmail.com', github:'https://github.com/JarrettChen217', linkedin:'https://www.linkedin.com/in/chen-hao-j33625/', instagram:'https://www.instagram.com/jarrettchen7/', instagramHandle:'@jarrettchen7', wechat:'Che8147',
   internships:[],
   selected: PROJECTS.selected,
   projects: PROJECTS.projects
